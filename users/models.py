25c0d29395c48d9e7d4e633bde0100ef62acac78
Mark Volkov <markasch67@gmail.com>
2025-08-21 22:56:38 +0300

    Added user and habit models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    email = models.EmailField(verbose_name="Email", unique=True)
+    phone = models.IntegerField(verbose_name="Номер телефона", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["username"]
+
+    def __str__(self):
+        return self.username
